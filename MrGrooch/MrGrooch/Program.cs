24878a588362977400f6c2c4594863fc48fd93cc
Reena <19yuanr@elmbrookstudents.org>
2018-06-06 10:46:59 -0500

    Initial setup

---
+++ MrGrooch/MrGrooch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MrGrooch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
